4f54cd43cf2a170d19937e120344d49257800500
Nathan <nat.hando@hotmail.com>
2021-10-02 19:33:27 -0300

    Cria model Produto

---
+++ CadastroProduto.API/Models/Produto.cs
@@ -0,0 +1,12 @@
+﻿namespace CadastroProduto.API.Models
+{
+    public class Produto
+    {
+        public string Id { get; set; }
+        public string Codigo { get; set; }
+        public string Descricao { get; set; }
+        public int CodigoDepartamento { get; set; }
+        public decimal Preco { get; set; }
+        public bool Status { get; set; }
+    }
+}
