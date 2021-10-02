9cf25f9d1646372ed7324caa37cf53c67f0aae96
Nathan <nat.hando@hotmail.com>
2021-10-02 19:31:31 -0300

    Cria ProdutoDbContext

---
+++ CadastroProduto.API/DbContexts/ProdutoDbContext.cs
@@ -0,0 +1,23 @@
+﻿using CadastroProduto.API.Models;
+using Microsoft.EntityFrameworkCore;
+namespace CadastroProduto.API
+{
+    public class ProdutoDbContext : DbContext
+    {
+        public ProdutoDbContext(DbContextOptions<ProdutoDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Produto> Produtos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Produto>().Property(p => p.Id).HasMaxLength(36).IsRequired();
+            modelBuilder.Entity<Produto>().Property(p => p.Codigo).HasMaxLength(10).IsRequired();
+            modelBuilder.Entity<Produto>().Property(p => p.Descricao).HasMaxLength(50).IsRequired();
+            modelBuilder.Entity<Produto>().Property(p => p.CodigoDepartamento).HasPrecision(10, 2).IsRequired();
+            modelBuilder.Entity<Produto>().Property(p => p.Preco).HasPrecision(10, 2).IsRequired();
+            modelBuilder.Entity<Produto>().Property(p => p.Status).IsRequired();
+        }
+    }
+}
