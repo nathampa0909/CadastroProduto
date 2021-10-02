d16adbc6bb256241eaa8e766cd7a82f29427990d
Nathan <nat.hando@hotmail.com>
2021-10-02 19:34:35 -0300

    Insere serviço do MySQL no Startup da api

--- CadastroProduto.API/Startup.cs
+++ CadastroProduto.API/Startup.cs
@@ -1,15 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace CadastroProduto.API
 {
@@ -25,6 +21,9 @@ namespace CadastroProduto.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            string mySqlConnection = Configuration.GetConnectionString("StringConnectionMySQL");
+
+            services.AddDbContextPool<ProdutoDbContext>(options => options.UseMySql(mySqlConnection, ServerVersion.AutoDetect(mySqlConnection)));
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
