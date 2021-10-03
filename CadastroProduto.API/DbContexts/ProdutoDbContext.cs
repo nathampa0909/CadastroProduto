5b94506885595a8eba62b4fdf41300c8a51eabc4
Nathan <nat.hando@hotmail.com>
2021-10-03 20:35:19 -0300

    Remove precisão de propriedade CodigoDepartamento

--- CadastroProduto.API/DbContexts/ProdutoDbContext.cs
+++ CadastroProduto.API/DbContexts/ProdutoDbContext.cs
@@ -15,7 +15,7 @@ namespace CadastroProduto.API
             modelBuilder.Entity<Produto>().Property(p => p.Id).HasMaxLength(36).IsRequired();
             modelBuilder.Entity<Produto>().Property(p => p.Codigo).HasMaxLength(10).IsRequired();
             modelBuilder.Entity<Produto>().Property(p => p.Descricao).HasMaxLength(50).IsRequired();
-            modelBuilder.Entity<Produto>().Property(p => p.CodigoDepartamento).HasPrecision(10, 2).IsRequired();
+            modelBuilder.Entity<Produto>().Property(p => p.CodigoDepartamento).HasMaxLength(10).IsRequired();
             modelBuilder.Entity<Produto>().Property(p => p.Preco).HasPrecision(10, 2).IsRequired();
             modelBuilder.Entity<Produto>().Property(p => p.Status).IsRequired();
         }
