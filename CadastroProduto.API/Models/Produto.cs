661db06563960e764273be89405b26efcb749631
Nathan <nat.hando@hotmail.com>
2021-10-03 20:39:07 -0300

    Altera tipo da propriedade CodigoDepartamento e insere tipo nulo no bool Status

--- CadastroProduto.API/Models/Produto.cs
+++ CadastroProduto.API/Models/Produto.cs
@@ -5,8 +5,8 @@
         public string Id { get; set; }
         public string Codigo { get; set; }
         public string Descricao { get; set; }
-        public int CodigoDepartamento { get; set; }
+        public string CodigoDepartamento { get; set; }
         public decimal Preco { get; set; }
-        public bool Status { get; set; }
+        public bool? Status { get; set; }
     }
 }
