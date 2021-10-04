53743efc4f803332316b85240fbe4bc82b6b2547
Nathan <nat.hando@hotmail.com>
2021-10-04 15:26:17 -0300

    Adiciona models Departamento e Produto

---
+++ CadastroProduto.Web/Models/DepartamentoModel.cs
@@ -0,0 +1,9 @@
+﻿namespace CadastroProduto.Web.Models
+{
+    public class DepartamentoModel
+    {
+        public string Id { get; set; }
+        public string Codigo { get; set; }
+        public string Nome { get; set; }
+    }
+}
---
+++ CadastroProduto.Web/Models/ProdutoModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CadastroProduto.Web.Models
+{
+    public class ProdutoModel
+    {
+        public string Id { get; set; }
+
+        [Display(Name = "Código")]
+        public string Codigo { get; set; }
+
+        [Display(Name = "Descrição")]
+        public string Descricao { get; set; }
+
+        [Display(Name = "Departamento")]
+        public string CodigoDepartamento { get; set; }
+
+        [Display(Name = "Preço")]
+        public decimal Preco { get; set; }
+
+        public bool? Status { get; set; }
+    }
+}
