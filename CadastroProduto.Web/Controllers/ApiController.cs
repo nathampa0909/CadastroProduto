1a17309c3481c89f47bddfd90e198a3c010ac45c
Nathan <nat.hando@hotmail.com>
2021-10-04 15:26:00 -0300

    Insere controllers da Api, página Home e página Produto

---
+++ CadastroProduto.Web/Controllers/ApiController.cs
@@ -0,0 +1,92 @@
+﻿using CadastroProduto.Web.Models;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace CadastroProduto.Web.Controllers
+{
+    public class ApiController
+    {
+        static HttpClient httpClient = new();
+        const string URL_API = "http://localhost:34070";
+
+        public async Task<List<ProdutoModel>> ObtenhaTodosProdutos()
+        {
+            var response = await httpClient.GetAsync(URL_API + "/api/produtos");
+
+            if (response.IsSuccessStatusCode)
+            {
+                return await response.Content.ReadAsAsync<List<ProdutoModel>>();
+            }
+
+            return null;
+        }
+
+        public async Task<ProdutoModel> ObtenhaProduto(string id)
+        {
+            var response = await httpClient.GetAsync(URL_API + "/api/produtos/" + id);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return await response.Content.ReadAsAsync<ProdutoModel>();
+            }
+
+            return null;
+        }
+
+        public async Task<List<DepartamentoModel>> ObtenhaDepartamentos()
+        {
+            var response = await httpClient.GetAsync("https://private-anon-9924aff2ed-maximatech.apiary-mock.com/fullstack/departamento");
+
+            if (response.IsSuccessStatusCode)
+            {
+                return await response.Content.ReadAsAsync<List<DepartamentoModel>>();
+            }
+
+            return null;
+        }
+
+        public async Task<HttpResponseMessage> InsiraProduto(ProdutoModel produto)
+        {
+            var produtoConvertido = JsonConvert.SerializeObject(produto);
+            var httpContent = new StringContent(produtoConvertido, Encoding.UTF8, "application/json");
+            var response = await httpClient.PostAsync(URL_API + "/api/produtos", httpContent);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return response;
+            }
+
+            return null;
+        }
+
+        public async Task<HttpResponseMessage> AtualizeProduto(ProdutoModel produto)
+        {
+            var produtoConvertido = JsonConvert.SerializeObject(produto);
+            var httpContent = new StringContent(produtoConvertido, Encoding.UTF8, "application/json");
+            var response = await httpClient.PutAsync(URL_API + "/api/produtos/" + produto.Id, httpContent);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return response;
+            }
+
+            return null;
+        }
+
+        public async Task<HttpResponseMessage> DeleteProduto(string id)
+        {
+            var response = await httpClient.DeleteAsync(URL_API + "/api/produtos/" + id);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return response;
+            }
+
+            return null;
+        }
+    }
+}
---
+++ CadastroProduto.Web/Controllers/ProdutoController.cs
@@ -0,0 +1,116 @@
+﻿using CadastroProduto.Web.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CadastroProduto.Web.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        readonly ApiController api = new();
+
+        public async Task<ActionResult> Index()
+        {
+            var produtos = await api.ObtenhaTodosProdutos(); 
+            var departamentos = await api.ObtenhaDepartamentos();
+
+            ViewBag.ListaDepartamentos = departamentos;
+
+            return View(produtos.OrderBy(produto => produto.Codigo));
+        }
+
+        public async Task<ActionResult> Criar()
+        {
+            var departamentos = await api.ObtenhaDepartamentos();
+            var selectListDepartamentos = new SelectList(departamentos, nameof(DepartamentoModel.Codigo), nameof(DepartamentoModel.Nome));
+
+            ViewBag.ListaDepartamentos = selectListDepartamentos;
+
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Criar(IFormCollection collection)
+        {
+            var produto = new ProdutoModel
+            {
+                Descricao = collection.First(c => c.Key.Equals("Descricao")).Value.ToString(),
+                CodigoDepartamento = collection.First(c => c.Key.Equals("CodigoDepartamento")).Value.ToString(),
+                Preco = decimal.Parse(collection.First(c => c.Key.Equals("Preco")).Value.ToString()),
+                Status = collection.First(c => c.Key.Equals("Status")).Value.ToString().Equals("0")
+            };
+
+            await api.InsiraProduto(produto);
+
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public async Task<ActionResult> Editar(string id)
+        {
+            var departamentos = await api.ObtenhaDepartamentos();
+            var selectListDepartamentos = new SelectList(departamentos, nameof(DepartamentoModel.Codigo), nameof(DepartamentoModel.Nome));
+
+            ViewBag.ListaDepartamentos = selectListDepartamentos;
+
+            var produto = await api.ObtenhaProduto(id);
+            return View(produto);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Editar(string id, IFormCollection collection)
+        {
+            var produto = new ProdutoModel
+            {
+                Id = id,
+                Codigo = collection.First(c => c.Key.Equals("Codigo")).Value.ToString(),
+                Descricao = collection.First(c => c.Key.Equals("Descricao")).Value.ToString(),
+                CodigoDepartamento = collection.First(c => c.Key.Equals("CodigoDepartamento")).Value.ToString(),
+                Preco = decimal.Parse(collection.First(c => c.Key.Equals("Preco")).Value.ToString()),
+                Status = collection.First(c => c.Key.Equals("Status")).Value.ToString().Equals("0")
+            };
+
+            await api.AtualizeProduto(produto);
+
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public async Task<ActionResult> Deletar(string id)
+        {
+            var produto = await api.ObtenhaProduto(id);
+            return View(produto);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Deletar(string id, IFormCollection collection)
+        {
+            await api.DeleteProduto(id);
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
